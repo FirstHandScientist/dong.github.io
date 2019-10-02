5bcf91ae6dfa0fe4374b927277560c5eee0338e6
Dong Liu <doli@kth.se>
2019-10-02 19:26:12 +0200

    add publications

---
+++ assets/auto/bibfile.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "bibfile"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "liuenergy"
+    "liu2019dominant"
+    "ren2015applying"
+    "zhang2015effective"
+    "wang2015structural"
+    "ren2015spectram"
+    "liu2018will"
+    "liu2019neural"
+    "liu2019discontinuous"
+    "liu2014secondary"
+    "liu2019alpha"
+    "zhang2014cooperative"
+    "ren2015spectrum"
+    "wang2015rsh"
+    "liu2015bounds"
+    "liu2015node"
+    "liu2019entropy"))
+ :bibtex)
+
