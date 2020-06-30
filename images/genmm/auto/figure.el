a28474a2c74595a4810da117dbb535f88c557fd4
Dong Liu <doli@kth.se>
2020-06-30 11:47:49 +0200

    include figs

---
+++ images/genmm/auto/figure.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "figure"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "landscape")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "tikz"
+    "bm"
+    "amsmath"
+    "graphicx")
+   (TeX-add-symbols
+    '("ubm" 1)
+    '("ubar" 1)
+    "midarrow"
+    "Ss"
+    "Tt"))
+ :latex)
+
